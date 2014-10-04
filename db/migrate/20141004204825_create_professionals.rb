ff25cd37f4bcdb16768eeb9e886e2d86558ad741
Elentras <mortelette.jeremy@gmail.com>
2014-10-04 23:06:30 +0200

    [scaffold] Add basics view for professionals

---
+++ db/migrate/20141004204825_create_professionals.rb
@@ -0,0 +1,18 @@
+class CreateProfessionals < ActiveRecord::Migration
+  def change
+    create_table :professionals do |t|
+      t.string :firstname
+      t.string :lastname
+      t.string :address_1
+      t.string :address_2
+      t.string :zipcode
+      t.string :city
+      t.string :country
+      t.string :latitude
+      t.string :longitude
+      t.string :speciality
+
+      t.timestamps
+    end
+  end
+end

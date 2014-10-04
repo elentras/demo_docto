15c03a0d1b0d45ab5a83298d4061447d9abc4862
Elentras <mortelette.jeremy@gmail.com>
2014-10-04 23:54:39 +0200

    [navigation] Add route and shared partial

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 DemoDocto::Application.routes.draw do
+  root to: "professionals#index"
+  resources :professionals
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

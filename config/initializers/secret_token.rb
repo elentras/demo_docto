a6a806ef8e569671d559f51709dab5a3f9174fb6
Elentras <mortelette.jeremy@gmail.com>
2014-10-04 23:05:09 +0200

    [rails] Add project's bases

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoDocto::Application.config.secret_key_base = 'fb3aaebb9f4f17eab223053f763064b5a3d77757c5b37aeac8c30ba16c8f44842a0e6628f0b8b5eb313f453dc12870076f07830bddf5c7e4bb64ef57fde2ddeb'
